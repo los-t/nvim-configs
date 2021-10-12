c93f528e3eb015b78a76d8eb691271c54ccfda6c
los-t <74127+los-t@users.noreply.github.com>
2021-10-13 00:34:12 +0300

    some cleanup and usability improvements
    - new Telescope bindings,
    - better guifont for nvy on my 4k 28".
    - abandon CtrlSF - I haven't used it in years

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@ vim.cmd [[filetype plugin on]]
 vim.cmd [[filetype indent on]]
 vim.cmd [[syntax on]]
 
-vim.o.guifont = "Fira Code Retina:h12"
+vim.o.guifont = "Fira Code:h14"
 vim.o.backspace = "indent,eol,start"
 
 vim.bo.shiftwidth = 2
@@ -43,6 +43,32 @@ vim.o.hidden = true
 
 vim.g.mapleader = " "
 
+-- Key Mappings
+
+local key_map = function(mode, lhs, rhs)
+  vim.api.nvim_set_keymap(mode, lhs, rhs, { noremap=true, silent=true })
+end
+
+key_map("n", "<leader>l", ":Telescope buffers<CR>")
+key_map("n", "<leader>f", ":Telescope git_files<CR>")
+key_map("n", "<leader>*", ":Telescope grep_string<CR>")
+
+key_map("n", "<leader>gb", ":Telescope git_bcommits<CR>")
+key_map("n", "<leader>gl", ":Telescope git_commits<CR>")
+key_map("n", "<leader>gs", ":Telescope git_stash<CR>")
+key_map("n", "<leader>gd", ":Telescope git_status<CR>")
+
+key_map("n", "<leader>r", ":Telescope lsp_references<CR>")
+
+key_map("n", "<leader>nh", ":nohls<CR>")
+key_map("n", "<leader>cd", ":lcd %:p:h<CR>")
+
+key_map("n", "<C-h>", "<C-w>h")
+key_map("n", "<C-j>", "<C-w>j")
+key_map("n", "<C-k>", "<C-w>k")
+key_map("n", "<C-l>", "<C-w>l")
+
+
 vim.api.nvim_exec([[
 "if exists("g:neovide")
 "  set guifont=Fira\ Code\ Retina:h16
@@ -55,13 +81,6 @@ if has("win32")
   set mouse=a
 endif
 
-"--------- Key Mappings -------
-"noremap <leader>l :ls<CR>:b<space>
-noremap <leader>l :Telescope buffers<CR>
-noremap <leader>f :Telescope git_files<CR>
-noremap <leader>nh :nohls<CR>
-noremap <leader>cd :lcd %:p:h<CR>
-
 map <F3> :echo "hi<" . synIDattr(synID(line("."),col("."),1),"name") . '> trans<' . synIDattr(synID(line("."),col("."),0),"name") . "> lo<" . synIDattr(synIDtrans(synID(line("."),col("."),1)),"name") . ">"<CR>
 
 " Man and Help
@@ -70,12 +89,6 @@ if has("unix")
   nmap <expr> <leader>m ':Man 3 '.expand('<cword>').'<cr>'
 endif
 "nmap <expr> <leader>h ':help '.expand('<cword>').'<cr>'
-
-" Window management
-nnoremap <C-h> <C-w>h
-nnoremap <C-j> <C-w>j
-nnoremap <C-k> <C-w>k
-nnoremap <C-l> <C-w>l
 
 " File browser
 let g:loaded_netrwPlugin = 1 " don't interfere with filebeagle
@@ -131,19 +144,6 @@ if has("unix")
   nnoremap <silent> <leader>v :call FzyCommand('rg --files .', ':vsplit ')<cr>
   nnoremap <silent> <leader>t :call FzyCommand('rg --files .', ':tabnew ')<cr>
 endif
-
-" Plugins
-let g:ctrlsf_default_root = "project"
-let g:ctrlsf_ackprg = "rg"
-let g:ctrlsf_auto_close = { "normal": 0, "compact": 0 }
-let g:ctrlsf_auto_focus = { "at": "start" }
-let g:ctrlsf_indent = 2
-"partial mapping override does not work for some reason
-"let g:ctrlsf_mapping = { "prev": "N", "next": "n" }
-nnoremap <leader>/ :CtrlSF 
-"nnoremap <silent> <leader>* :CtrlSF<cr>
-"nnoremap <silent> <leader>f :CtrlSFToggle<cr>
-nnoremap <silent> <leader>* :Telescope grep_string<cr>
 
 packloadall
 silent! helptags ALL
