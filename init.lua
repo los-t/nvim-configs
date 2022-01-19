094bdfc31759e657f72a4c1a4d15b7440cbe53da
los-t <74127+los-t@users.noreply.github.com>
2022-01-19 03:10:27 +0300

    cleanup: move mappings to their own keymap.lua
    ...and rename 'key_map' to 'bind' in the process, which reads better.
    This way things look a bit more structured.

--- init.lua
+++ init.lua
@@ -47,53 +47,7 @@ if vim.fn.has("win32") then
 end
 
 require('plugins')
-
--- Key Mappings
-
-local key_map = function(mode, lhs, rhs)
-  vim.api.nvim_set_keymap(mode, lhs, rhs, { noremap=true, silent=true })
-end
-
-key_map("n", "<leader>l", ":Telescope buffers<CR>")
-key_map("n", "<leader>f", ":Telescope git_files<CR>")
-key_map("n", "<leader>*", ":Telescope grep_string<CR>")
-
-key_map("n", "<leader>gb", ":Telescope git_bcommits<CR>")
-key_map("n", "<leader>gl", ":Telescope git_commits<CR>")
-key_map("n", "<leader>gs", ":Telescope git_stash<CR>")
-key_map("n", "<leader>gd", ":Telescope git_status<CR>")
-
-key_map("n", "<leader>r", ":Telescope lsp_references<CR>")
-
-key_map("n", "<leader>nh", ":nohls<CR>")
-key_map("n", "<leader>cd", ":lcd %:p:h<CR>")
-
-key_map("n", "<C-h>", "<C-w>h")
-key_map("n", "<C-j>", "<C-w>j")
-key_map("n", "<C-k>", "<C-w>k")
-key_map("n", "<C-l>", "<C-w>l")
-
-key_map("n", "gd", "<cmd>lua vim.lsp.buf.declaration()<CR>")
-key_map("n", "<C-]>", "<cmd>lua vim.lsp.buf.definition()<CR>")
-key_map("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>")
-key_map("n", "gD", "<cmd>lua vim.lsp.buf.implementation()<CR>")
-key_map("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>")
-key_map("n", "1gD", "<cmd>lua vim.lsp.buf.type_definition()<CR>")
-key_map("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>")
-key_map("n", "g0", "<cmd>lua vim.lsp.buf.document_symbol()<CR>")
-key_map("n", "<leader>o", "<cmd>lua vim.diagnostic.open_float(0, {scope=\"line\", border=\"single\"})<CR>")
-
-key_map("n", "-", ":FileBeagleBufferDir<CR>")
-key_map("n", "<leader>e", ":vsplit<bar>FileBeagleBufferDir<CR>")
-
-key_map("t", "<Esc>", "<C-\\><C-n>")
-if (vim.fn.has("win32")) then
-  key_map("n", "<leader>tb", ":vsplit<bar>term git-cmd.exe")
-  key_map("n", "<leader>tw", ":vsplit<bar>term<CR>i")
-  key_map("n", "<leader>tl", ":vsplit<bar>term bash -i<CR>i")
-else
-  key_map("n", "<leader>tb", ":vsplit<bar>term<CR>i")
-end
+require('keymap')
 
 vim.api.nvim_exec([[
 if exists("g:neovide")
---
+++ lua/keymap.lua
@@ -0,0 +1,44 @@
+local bind = function(mode, lhs, rhs)
+  vim.api.nvim_set_keymap(mode, lhs, rhs, { noremap=true, silent=true })
+end
+
+bind("n", "<leader>l", ":Telescope buffers<CR>")
+bind("n", "<leader>f", ":Telescope git_files<CR>")
+bind("n", "<leader>*", ":Telescope grep_string<CR>")
+
+bind("n", "<leader>gb", ":Telescope git_bcommits<CR>")
+bind("n", "<leader>gl", ":Telescope git_commits<CR>")
+bind("n", "<leader>gs", ":Telescope git_stash<CR>")
+bind("n", "<leader>gd", ":Telescope git_status<CR>")
+
+bind("n", "<leader>r", ":Telescope lsp_references<CR>")
+
+bind("n", "<leader>nh", ":nohls<CR>")
+bind("n", "<leader>cd", ":lcd %:p:h<CR>")
+
+bind("n", "<C-h>", "<C-w>h")
+bind("n", "<C-j>", "<C-w>j")
+bind("n", "<C-k>", "<C-w>k")
+bind("n", "<C-l>", "<C-w>l")
+
+bind("n", "gd", "<cmd>lua vim.lsp.buf.declaration()<CR>")
+bind("n", "<C-]>", "<cmd>lua vim.lsp.buf.definition()<CR>")
+bind("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>")
+bind("n", "gD", "<cmd>lua vim.lsp.buf.implementation()<CR>")
+bind("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>")
+bind("n", "1gD", "<cmd>lua vim.lsp.buf.type_definition()<CR>")
+bind("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>")
+bind("n", "g0", "<cmd>lua vim.lsp.buf.document_symbol()<CR>")
+bind("n", "<leader>o", "<cmd>lua vim.diagnostic.open_float(0, {scope=\"line\", border=\"single\"})<CR>")
+
+bind("n", "-", ":FileBeagleBufferDir<CR>")
+bind("n", "<leader>e", ":vsplit<bar>FileBeagleBufferDir<CR>")
+
+bind("t", "<Esc>", "<C-\\><C-n>")
+if (vim.fn.has("win32")) then
+  bind("n", "<leader>tb", ":vsplit<bar>term git-cmd.exe")
+  bind("n", "<leader>tw", ":vsplit<bar>term<CR>i")
+  bind("n", "<leader>tl", ":vsplit<bar>term bash -i<CR>i")
+else
+  bind("n", "<leader>tb", ":vsplit<bar>term<CR>i")
+end
