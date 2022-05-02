0f5ba0a2e4390582f9b94b7a8c49ebf495651253
los-t <74127+los-t@users.noreply.github.com>
2022-05-02 06:39:17 +0300

    completion: revise nvim-cmp bindings after update
    There was a breaking change that removed all the default bindings, so
    C-n/C-p had to be added. Also, I don't remember actually using Tab key
    for navigation therefore it was removed.

--- lua/config/completion.lua
+++ lua/config/completion.lua
@@ -12,38 +12,14 @@ cmp.setup({
     { name = 'nvim_lsp' },
     { name = 'luasnip' },
   }),
-  mapping = { -- copy-pasted from example
-    ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-    ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-    ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-    ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-    ['<C-e>'] = cmp.mapping({
-      i = cmp.mapping.abort(),
-      c = cmp.mapping.close(),
-    }),
+  mapping = cmp.mapping.preset.insert({ -- copy-pasted from example
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
     ['<CR>'] = cmp.mapping.confirm({ select = true }),
-
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      elseif has_words_before() then
-        cmp.complete()
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-  },
+    ['<C-n'] = cmp.mapping.select_next_item(),
+    ['<C-p'] = cmp.mapping.select_prev_item(),
+  }),
 })
 
