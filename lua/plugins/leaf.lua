11cc698249d78e8db8466f1deda2d079ae7d8571
los-t <74127+los-t@users.noreply.github.com>
2023-12-04 22:34:17 +0100

    colors: better, darker diff backgrounds
    Makes diffs actually readable without extra strain. Defaults were
    too bright and did not contrast with text enough.

--- lua/plugins/leaf.lua
+++ lua/plugins/leaf.lua
@@ -9,6 +9,10 @@ return {
         contrast = 'high',
         overrides = {
           Warning = { fg = leaf_colors.yellow_darker },
+          DiffAdd = { bg = leaf_colors.green_darker },
+          DiffText = { bg = leaf_colors.yellow_darker },
+          DiffChange = { bg = leaf_colors.blue_darker },
+          DiffDelete = { bg = leaf_colors.red_darker },
         }
       })
       vim.cmd([[colorscheme leaf]])
