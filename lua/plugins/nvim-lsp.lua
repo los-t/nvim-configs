ca69f7de22ee4173786d2dfe74cec91c4fec279d
los-t <74127+los-t@users.noreply.github.com>
2024-10-26 09:34:33 +0200

    add lua_ls to LSP's config
    Requires luals in $PATH (see https://luals.github.io/#neovim-install)

--- lua/plugins/nvim-lsp.lua
+++ lua/plugins/nvim-lsp.lua
@@ -16,5 +16,36 @@ return {
       capabilities = caps,
     }
     require'lspconfig'.tsserver.setup{ capabilities = caps }
+    require'lspconfig'.lua_ls.setup{
+      on_init = function(client)
+        if client.workspace_folders then
+          local path = client.workspace_folders[1].name
+          if vim.uv.fs_stat(path..'/.luarc.json') or vim.uv.fs_stat(path..'/.luarc.jsonc') then
+            return
+          end
+        end
+
+        client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+          runtime = {
+            -- Tell the language server which version of Lua you're using
+            -- (most likely LuaJIT in the case of Neovim)
+            version = 'LuaJIT'
+          },
+          -- Make the server aware of Neovim runtime files
+          workspace = {
+            preloadFileSize = 65536,
+            checkThirdParty = false,
+            library = {
+              vim.env.VIMRUNTIME,
+              "${3rd}/luv/library",
+              "${3rd}/busted/library",
+            }
+          }
+        })
+      end,
+      settings = {
+        Lua = {}
+      }
+    }
   end,
 }
